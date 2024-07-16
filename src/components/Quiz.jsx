8b3b41e3f67a7c82a1715f26d17b43628858c1f4
Ragul <ragulraja2808@gmail.com>
2024-07-16 15:41:40 +0530

    "Add initial Quiz component with state management"

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react"
 export default function Quiz() {
+    const [activeQuestionIndex, setActiveQuestionIndex] = useState([]);
+    const [userAnswer, setUserAnswer] = useState([])
     return <p>Currentlyactive Question</p>
 }
