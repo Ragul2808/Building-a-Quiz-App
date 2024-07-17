9d149b0984f68fdb348162d1a459e66625c86355
Ragul <ragulraja2808@gmail.com>
2024-07-17 07:27:08 +0530

    Add quiz completion summary and shuffle answers for each question

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,23 +1,35 @@
 import { useState } from "react";
 import QUESTIONS from '../questions.js';
+import quizIsCompleteImg from '../assets/quiz-complete.png'
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+    const quizIsCommplete = activeQuestionIndex === QUESTIONS.length;
 
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevUserAnswers) => {
             return [...prevUserAnswers, selectedAnswer];
         });
     }
+    if (quizIsCommplete) {
+        return <div id="summary">
+            <img src={quizIsCompleteImg} alt="Trophy icon"/>
+            <h2>Quiz Completed!</h2>
+        </div>;
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(()=> Math.random() -0.5);
+
 
     return (
         <div id="quiz">
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                    {shuffledAnswers.map((answer) => (
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                         </li>
